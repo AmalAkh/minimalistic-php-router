77b366bcaa01a558795d8b2bac7319351ed8dd07
root <root@3369391-dl78130.twc1.net>
2024-09-09 19:18:30 +0300

    App functionality added

--- App.php
+++ App.php
@@ -7,7 +7,24 @@ class App extends Router
 {
     function __construct()
     {
-        parent::__construct("");
+        $this->path = join_paths(str_replace($_SERVER["DOCUMENT_ROOT"],"", getcwd()));
+      
+    }
+    public function run()
+    {
+        
+        $requestedURI = str_replace($this->path, "",$_SERVER["REQUEST_URI"]);
+        var_dump($this->handlers);
+        if(!isset($this->handlers[$requestedURI]) || !isset($this->handlers[$requestedURI][$_SERVER["REQUEST_METHOD"]]))
+        {
+            http_response_code(404);
+            die("Path was not found");
+            return;
+        }
+        foreach($this->handlers[$requestedURI][$_SERVER["REQUEST_METHOD"]] as $handler)
+        {
+            $handler();
+        }
     }
 }
 ?>
