b0fb0a7fe14df9b055bf6b8e6029352ea6b6285e
root <root@3369391-dl78130.twc1.net>
2024-09-09 12:44:16 +0300

    Different kinds of requests were added

--- abstractions/Router.php
+++ abstractions/Router.php
@@ -4,18 +4,19 @@ class Router
 {
     protected $path = "";
     private $handlers = [];
-    function __construct($path)
+    function __construct($path = "")
     {
-        $this->path = join_paths(str_replace($_SERVER["DOCUMENT_ROOT"],"",getcwd()),$path);
+        $this->path = join_paths(str_replace($_SERVER["DOCUMENT_ROOT"],"", getcwd()),$path);
     }
     /**
      * @param string $method HTTP method
      * @param string $path path for the request
      * @param mixed[] $handlers handlers for the reqest
-     * 
+     * Adds new route
      */
     function route($method, $path, ...$handlers)
     {   
+       
         $path = join_paths($this->path, $path);
         $method = strtoupper($method);
         if(!isset($this->handlers[$path]))
@@ -34,13 +35,42 @@ class Router
             array_push($this->handlers[$path][$method], $handler);
         }
     }
-
+    function get($path, ...$handlers)
+    {
+        $this->route("GET", $path, ...$handlers);
+    }
+    function post($path, ...$handlers)
+    {
+        $this->route("POST", $path, ...$handlers);
+    }
+    function patch($path, ...$handlers)
+    {
+        $this->route("PATCH", $path, ...$handlers);
+    }
+    function put($path, ...$handlers)
+    {
+        $this->route("PUT", $path, ...$handlers);
+    }
+    function delete($path, ...$handlers)
+    {
+        $this->route("DELETE", $path, ...$handlers);
+    }
+    function head($path, ...$handlers)
+    {
+        $this->route("HEAD", $path, ...$handlers);
+    }
+    
+    /**
+     * Handle current route
+     */
     function run()
     {
-       
+        
         if(!isset($this->handlers[$_SERVER["REQUEST_URI"]]) || !isset($this->handlers[$_SERVER["REQUEST_URI"]][$_SERVER["REQUEST_METHOD"]]))
         {
             http_response_code(404);
+            die("Path was not found");
+            return;
         }
         foreach($this->handlers[$_SERVER["REQUEST_URI"]][$_SERVER["REQUEST_METHOD"]] as $handler)
         {
